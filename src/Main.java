e2b7244805a41f6f0937cddafa0a681660202972
ChenTipCalculator <bonniec15@nycstudents.net>
2022-09-22 10:54:37 -0400

    -_-

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import java.text.DecimalFormat;
         Scanner scan = new Scanner(System.in);
         DecimalFormat formatter = new DecimalFormat("#.##");
 
+    //Welcoming Statement
         System.out.println("Welcome to TipCalculator!");
 
         System.out.print("How many people are in your group?: ");
@@ -16,17 +17,25 @@ import java.text.DecimalFormat;
         int tipPercent = scan.nextInt();
         scan.nextLine();
 
+      //Use of while loop to track prices of items
 
         System.out.println("Enter cost in dollars and cents ex: 4.50  (-1 to end):");
         double priceOfItem = scan.nextDouble();
+        scan.nextLine();
+        double billBeforeTip = 0;
+        billBeforeTip = billBeforeTip + priceOfItem;
+
 
         while (priceOfItem != -1) {
             System.out.println("Enter cost in dollars and cents ex: 4.50  (-1 to end):");
             priceOfItem = scan.nextDouble();
+            billBeforeTip = billBeforeTip + priceOfItem;
         }
+            billBeforeTip = billBeforeTip + 1;
 
-
-
+       //Receipt
+        System.out.println("____________________________");
+        System.out.println(billBeforeTip);
 
 
     }
